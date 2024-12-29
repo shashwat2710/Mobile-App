6723e20adad6a553d6c7d099f06ea7dbcc5ed040
shashwat2710 <40847974+shashwat2710@users.noreply.github.com>
2024-12-29 14:21:36 +0530

    CORS policy added

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const https = require('https');
+const http = require('http');
 const fs = require('fs');
+const cors = require('cors');
 const bodyParser = require('body-parser');
+
+//Routes
 const signupRoutes = require('./routes/signup');
 const loginRoutes = require('./routes/login');
 const generateOTPRoutes = require('./routes/generate-otp');
 const verifyOTPRoutes = require('./routes/verify-otp');
 
 const app = express();
-const PORT = 5000;
+const PORT_HTTP = 5000; // HTTP port
+const PORT_HTTPS = 5443; // HTTPS port
 
 // Load SSL Certificates
 const options = {
@@ -16,7 +21,12 @@ const options = {
   cert: fs.readFileSync('./cert/cert.pem'), // Path to your certificate
 };
 
+const corsOptions = {
+  origin:"http://localhost:8081",
+}
+
 // Middleware
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // Routes
@@ -26,6 +36,10 @@ app.use('/api', generateOTPRoutes);
 app.use('/api', verifyOTPRoutes);
 
 // Start the Server
-https.createServer(options, app).listen(PORT, () => {
-  console.log(`Secure server running at https://localhost:${PORT}`);
+https.createServer(options, app).listen(PORT_HTTPS, () => {
+  console.log(`Secure server running at https://localhost:${PORT_HTTPS}`);
+});
+
+http.createServer(app).listen(PORT_HTTP, () => {
+  console.log(`HTTP server running at http://localhost:${PORT_HTTP}`);
 });
